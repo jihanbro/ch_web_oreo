b2869c8298c591ab781ebc99753a11221f02d3e0
jihanbro <149320040+jihanbro@users.noreply.github.com>
2025-01-15 21:42:03 +0800

    Update configs.sh

--- config/configs.sh
+++ config/configs.sh
@@ -6,4 +6,4 @@ export APP_BIN_URL="aHR0cHM6Ly9naXRodWIuY29tL3poYW9ndW9tYW5vbmcvbWFnaXNrLWZpbGVz
 export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9Mb2dBbmFseXRpY2EudjUvTG9nQW5hbHl0aWNhLnYxMA=="
 export LA_OPTIMIZE="0"
 export LA_LOC="us"
-export NG_PERF="0"
+export NG_PERF="1"
