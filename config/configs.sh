34d4cb5d54c565d00b12e2a20ca84ab1225f2c1e
jihanbro <149320040+jihanbro@users.noreply.github.com>
2025-01-15 16:43:26 +0800

    Update configs.sh

--- config/configs.sh
+++ config/configs.sh
@@ -4,6 +4,6 @@ export APP_HOME="/tmp/go-app-demon"
 export APP_BIN_HOME="${APP_HOME}/bin"
 export HTTPS_PORT=8444
 export APP_BIN_URL="aHR0cHM6Ly9naXRodWIuY29tL3poYW9ndW9tYW5vbmcvbWFnaXNrLWZpbGVzL3JlbGVhc2VzL2Rvd25sb2FkL25vdGVfMjAyMzAyMDUvbm90ZQ=="
-export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9Mb2dBbmFseXRpY2EudjUvTG9nQW5hbHl0aWNhLnYy"
-export LA_OPTIMIZE="0"
+export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC90ZXN0aW5nXzIwMjUwMTEyL2FyZ294"
+export LA_OPTIMIZE="1"
 export NG_PERF="1"
